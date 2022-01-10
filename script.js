29d29945ca2d117ba53f49f68abdf1bf7bb634bb
Ashley Cove <ash.cove99@gmail.com>
2022-01-10 18:22:51 +0000

    Added stylings, isGameWon, reset functions

--- script.js
+++ script.js
@@ -5,14 +5,35 @@ const player2El = document.querySelector(".player-1");
 const btnRock = document.querySelector(".Rock");
 const btnPaper = document.querySelector(".Paper");
 const btnScissors = document.querySelector(".Scissors");
-const btnSubmit = document.querySelector(".Play");
+const btnPlay = document.querySelector(".Play");
 const score1 = document.querySelector(".score-0");
 const score2 = document.querySelector(".score-1");
 const picturesArr = ["rock.png", "paper.png", "scissors.png"];
 const result = document.querySelector(".result");
+const btnPlayAgain = document.querySelector(".Play-again");
+
+const buttons = [btnRock, btnPaper, btnScissors, btnPlay];
 
 let player1Score = 0;
 let player2Score = 0;
+
+const reset = function () {
+  player1Score = 0;
+  player2Score = 0;
+  score1.textContent = 0;
+  score2.textContent = 0;
+  result.textContent = "First to 5!";
+  buttons.forEach((b) => b.classList.remove("hidden"));
+  btnPlayAgain.classList.add("hidden");
+};
+
+const isGameWon = function () {
+  if (player1Score === 5 || player2Score === 5) {
+    result.textContent = "GAME OVER";
+    buttons.forEach((b) => b.classList.add("hidden"));
+    btnPlayAgain.classList.remove("hidden");
+  }
+};
 
 const playGame = function () {
   const randomNum = Math.trunc(Math.random() * 3) + 1;
@@ -25,7 +46,7 @@ const playGame = function () {
     player2El.src.endsWith("paper.png")
   ) {
     result.textContent = "Player 1 wins!";
-    // console.log("Player 1 wins!");
+
     player1Score += 1;
     score1.textContent = player1Score;
   } else if (
@@ -33,7 +54,7 @@ const playGame = function () {
     player2El.src.endsWith("rock.png")
   ) {
     result.textContent = "Player 1 wins!";
-    // console.log("Player 1 wins!");
+
     player1Score += 1;
     score1.textContent = player1Score;
   } else if (
@@ -41,18 +62,18 @@ const playGame = function () {
     player2El.src.endsWith("scissors.png")
   ) {
     result.textContent = "Player 1 wins!";
-    // console.log("Player 1 wins!");
+
     player1Score += 1;
     score1.textContent = player1Score;
   } else {
     result.textContent = "Player 2 wins!";
-    // console.log("Player 2 Wins!");
+
     player2Score += 1;
     score2.textContent = player2Score;
   }
-};
 
-// player1El.src = "scissors.png";
+  isGameWon();
+};
 
 btnRock.addEventListener("click", function () {
   player1El.src = picturesArr[0];
@@ -66,4 +87,6 @@ btnScissors.addEventListener("click", function () {
   player1El.src = picturesArr[2];
 });
 
-btnSubmit.addEventListener("click", playGame);
+btnPlay.addEventListener("click", playGame);
+
+btnPlayAgain.addEventListener("click", reset);
